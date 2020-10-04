4fad860f11872dd701b40a28dff2ea52db3dfdaf
DevJun <guswns1659@gmail.com>
2020-10-04 22:02:23 +0900

    feat : h2 db 연결

---
+++ src/main/java/com/titanic/dddstart/domain/MemberId.java
@@ -0,0 +1,12 @@
+package com.titanic.dddstart.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class MemberId implements Serializable {
+
+    @Column(name = "member_id")
+    private String id;
+}
---
+++ src/main/java/com/titanic/dddstart/domain/Order.java
@@ -0,0 +1,20 @@
+package com.titanic.dddstart.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "purchase_order")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_no")
+    private Long id;
+
+    @Embedded
+    private Orderer orderer;
+
+//    @Embedded
+//    private ShippingInfo shippingInfo;
+
+}
